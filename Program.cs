3c98626cfdf238a864fe3038adc6bafe75bc966d
Julian DeVries <DeVrJuli784@GoTVDSB.ca>
2021-10-21 14:13:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kick__Push
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
